b44410e7031ebe9aaa16dfa637cf3d3adcd58b0f
Benjamin <guangtun.ben.zhu@gmail.com>
2014-11-28 02:09:44 -0500

    Overhaul of hstfos_spec.py

--- Spectroscopy/hstfos_spec.py
+++ Spectroscopy/hstfos_spec.py
@@ -1,127 +1,230 @@
 
 """ 
-Useful small tools for spectroscopic analysis
+
 """
 
+from __future__ import division
+
 import numpy as np
-import spec_utils
-import spectrograph
 import fitsio
-import os
+import datapath
+from os.path import isfile, join
+from specutils import get_loglam
 from progressbar import ProgressBar
-import numpy.lib.recfunctions as recfunctions
 from pydl.pydlspec2d.spec2d import combine1fiber
 
-pbar = ProgressBar()
 
-astrodata_path = {'HSTFOS': os.path.join(os.environ['HOME'], 'AstroData/Quasars/HSTFOS'),
-                  'HSTCOS': os.path.join(os.environ['HOME'], 'AstroData/Quasars/HSTCOS')}
-
-savewave = False
-# Read in the master catalog
-basepath = astrodata_path['HSTFOS']
-infile = os.path.join(basepath, 'table1_master.fits')
-qso = fitsio.read(infile)
-
-# The gratings
-FOS_gratings = (spectrograph.FOSG130H, spectrograph.FOSG190H, spectrograph.FOSG270H)
-
-# Wavelength bins
-# Master bin (from 900. to 14400.)
-master_loglam = spec_utils.get_loglambda()
-# G130H wavelength bin
-FOSG130H_blue_index = np.searchsorted(master_loglam, np.log10(FOS_gratings[0].minwave))
-FOSG130H_red_index = np.searchsorted(master_loglam, np.log10(FOS_gratings[0].maxwave+FOS_gratings[0].width), side='Right')
-#FOSG130H_loglam = master_loglam[FOSG130H_blue_index:FOSG130H_red_index]
-# G190H wavelength bin
-FOSG190H_blue_index = np.searchsorted(master_loglam, np.log10(FOS_gratings[1].minwave))
-FOSG190H_red_index = np.searchsorted(master_loglam, np.log10(FOS_gratings[1].maxwave+FOS_gratings[1].width), side='Right')
-#FOSG190H_loglam = master_loglam[FOSG190H_blue_index:FOSG190H_red_index]
-# G270H wavelength bin
-FOSG270H_blue_index = np.searchsorted(master_loglam, np.log10(FOS_gratings[2].minwave))
-FOSG270H_red_index = np.searchsorted(master_loglam, np.log10(FOS_gratings[2].maxwave+FOS_gratings[2].width), side='Right')
-#FOSG270H_loglam = master_loglam[FOSG270H_blue_index:FOSG270H_red_index]
-
-# Use slicing to create views
-new_index = ((FOSG130H_blue_index, FOSG130H_red_index), 
-                    (FOSG190H_blue_index, FOSG190H_red_index),
-                    (FOSG270H_blue_index, FOSG270H_red_index))
-new_size = FOSG270H_red_index - FOSG130H_blue_index
-
-basefile = ['h130.fits', 'h190.fits', 'h270.fits']
-out_basefile = 'HSTFOS_commonwave_spec.fits'
-
-# set up outputs
-tmp_formats = "f8, f8"
-out_formats = tmp_formats.replace('f', '('+str(new_size)+',)f')
-out_names = ['flux', 'ivar']
-out_dtype = np.dtype({'names':out_names, 'formats':out_formats.split(', ')})
-tmp_outstr = np.zeros(1, dtype=out_dtype)
-
-# Save the common wavelength grid information
-if savewave: 
-   outwave = np.zeros(1, dtype=[('wave','f8', (master_loglam.size,)),('min_index','i4'),('max_index','i4')])
-   outwave['wave'] = np.power(10., master_loglam)
-   outwave['min_index'] = FOSG130H_blue_index 
-   outwave['max_index'] = FOSG270H_red_index
-   wave_outfile = os.path.join(basepath, 'HSTFOS_commonwave_wave.fits')
-   fits = fitsio.FITS(wave_outfile, 'rw', clobber=True)
-   fits.write(outwave)
-   fits.close()
-
-# Interpolation + Stitching (no Stitching yet)
-# for i in np.arange(qso.size):
-for i in np.arange(10):
-    # Progress bar
-    pbar.update(i+1)
-
-    # Sub-directory for the quasar
-    subpath = ((qso[i]['Name'].strip()).replace('+','p')).replace('-','m')
-
-    # Output file
-    thisqso_outfile = os.path.join(basepath, subpath, out_basefile)
-
-    # Initialization of temporary arrays
-    newflux = np.zeros((3, new_size))
-    newivar = np.zeros((3, new_size))
-
-    for j in np.arange(len(basefile)):
-        thisqso_infile = os.path.join(basepath, subpath, basefile[j])
-
-        if os.path.isfile(thisqso_infile): 
-           # Read in
-           indata = fitsio.read(thisqso_infile)
-           inivar = 1./(indata[0]['error'])**2
-           inivar = np.where(np.isfinite(inivar),inivar,0.0)
-
-           # Interpolation
-           tmp_flux, tmp_ivar=combine1fiber(np.log10(indata[0]['wave']), indata[0]['flux'], 
-               objivar=inivar, newloglam=master_loglam[new_index[j][0]:new_index[j][1]])
-#              objivar=1./(indata[0]['error'])**2, newloglam=master_loglam[new_index[j][0]:new_index[j][1]])
-
-           # Assignment
-           left_index = new_index[j][0] - new_index[0][0]
-           right_index = new_index[j][1] - new_index[0][0]
-           newflux[j, left_index:right_index] = tmp_flux
+# specutils
+def simple_coadd(flux, axis, weight=None):
+    if weight is None: 
+       weight = np.where(np.isfinite(flux), 1., 0.)
+    outweight = np.sum(weight, axis=axis)
+    outweight = np.where((np.isfinite(outweight) and outweight>0.), outweight, 0.)
+    outflux = np.sum(flux*weight, axis=axis)/(outweight + (outweight==0.))
+    return outflux, outweight
 
 
-           newivar[j, left_index:right_index] = tmp_ivar
+# allinone utils
+def allinone_wave_filename():
+    path = datapath.allinone_path()
+    filename = 'AIO_CommonWave.fits'
+    return join(path, filename)
 
-    # Stitch/Co-add spectra. Simple ivar-weighted mean. Likely need to revisit.
-    # Bottleneck?
-    # Also assuming most of objects have multiple spectra, otherwise should co-add only a portion of the spectra
-    outivar = np.sum(newivar, axis=0)
-    # Will using masked_array be faster?
-    outflux = np.sum(newflux*newivar, axis=0)/(outivar + (outivar == 0.))*(outivar > 0.)
-    outivar = outivar*(outivar > 0.)
 
-    # Output
-    tmp_outstr['flux'] = outflux
-    tmp_outstr['ivar'] = outivar
-    outstr = recfunctions.merge_arrays([qso[j], tmp_outstr], flatten=True, usemask=False)
+def allinone_wave_readin():
+    infile = allinone_wave_filename()
+    if not isfile(infile):
+       print ("{0} does not exist! \n" 
+              "Check the path or call allinone_wave_make() first.".format(infile))
+       return -1
+    return fitsio.read(infile)
 
-    # Write out output
-    fits = fitsio.FITS(thisqso_outfile, 'rw', clobber=True)
+
+def allinone_wave_make(overwrite=False):
+    outfile = allinone_wave_filename()
+    if (isfile(outfile) and not overwrite):
+       print "File exists. Use overwrite to overwrite it."
+       return -1
+    master_loglam = get_loglam(minwave=448., maxwave=10402., dloglam=1.E-4)
+    master_wave = np.power(10., master_loglam)
+    # preferred structured array
+    outstr = np.zeros(1, dtype=[('wave','f8', (master_loglam.size,))])
+    # Using outstr[0]['wave'] does not work, only the first element got the value, why? Memory layout conflict?
+    # Or shape conflict (1, n) vs. (n,)
+    outstr[0]['wave'][:] = np.power(10., master_loglam)
+    fits = fitsio.FITS(outfile, 'rw', clobber=True)
     fits.write(outstr)
     fits.close()
+
+
+def allinone_wavebase(band):
+    wavebase = {'EUV': [450.,900.],
+                'FUV': [900.,1800.],
+                'NUV': [1800.,3600.],
+                'OPTICAL': [3600.,7200.],
+                'NIR': [7200.,10400.]
+               }.get(band)
+    return wavebase
+
+
+def allinone_filebase(band):
+    filebase = {'EUV': 'Wave00450_00900A',
+                'FUV': 'Wave00900_01800A',
+                'NUV': 'Wave01800_03600A',
+                'OPTICAL': 'Wave03600_07200A',
+                'NIR': 'Wave07200_10400A'
+               }.get(band)
+    return filebase
+
+
+# hstfos utils
+hstfos_allinone_observer_bands = ['FUV', 'NUV']
+hstfos_allinone_rest_bands     = ['EUV', 'FUV', 'NUV']
+hstfos_allinone_observer_fileprefix = 'AIO_QSO_HSTFOS_ObserverFrame_'
+hstfos_allinone_rest_fileprefix     = 'AIO_QSO_HSTFOS_NEDzRestFrame_'
+
+
+def hstfos_allinone_rest_filename(band):
+    path = datapath.allinone_path()
+    filebase = allinone_rest_filebase(band)
+    filename = join(path, hstfos_allinone_rest_fileprefix+filebase+'.fits')
+    return join(path, filename)
+
+
+def hstfos_allinone_observer_filename(band):
+    path = datapath.allinone_path()
+    filebase = allinone_observer_filebase(band)
+    filename = hstfos_allinone_observer_fileprefix+filebase+'.fits'
+    return join(path, filename)
+
+
+def hstfos_readspec(obj, channel, status=None):
+    """Read in an HST FOS quasar spectrum
+    """
+    path = datapath.hstfos_path()
+    subpath = ((obj['Name'].strip()).replace('+','p')).replace('-','m')
+    filename = channel+'.fits'
+    infile = join(path, subpath, filename)
+    print infile
+    if isfile(infile) is True:
+       status=1
+       return fitsio.read(infile)
+    status=-1
+    return status
+
+
+def hstfos_allinone_rest_writeout(qso, wave, flux, continuum, ivar, overwrite=False):
+    """
+    Saving continuum as well.
+    """
+
+    bands = hstfos_allinone_restframe_bands
+    for thisband in bands:
+        # check outfiles
+        outfile = hstfos_allinone_restframe_filename(thisband)
+        if isfile(thisfile) and not overwrite:
+           print "File {0} exists. Use overwrite to overwrite it.".format(outfile)
+        print "Will write into these files: {0}".format(outfile)
+        wavebase = allinone_rest_wavebase(thisband)
+
+        # wavelength range
+        index_wave = np.searchsorted(wave, wavebase)
+        nwave = index_wave[1] - index_wave[0]
+        # quasars with flux in the range
+        index_qso = (where((qso.z > (1100./wave[index_wave[1]]-1.-0.001)) and (qso.z <= (3350./wave[index_wave[0]]-1.+0.001))))[0]
+        if index_qso.size>0:
+           outstr_dtype = [('index_qso', 'i4', (index_qso.size,)), 
+                           ('ra', 'f8', (index_qso.size,)), ('dec', 'f8', (index_qso.size,)), ('z', 'f4', (index_qso.size,)),
+                           ('wave', 'f4', (nwave,)), 
+                           ('flux', 'f4', (nwave,)), 
+                           ('continuum', 'f4', (nwave,)), 
+                           ('ivar', 'f4', (nwave,))] 
+           outstr = np.array([(index_qso,
+                               qso[index_qso]['ra'], qso[index_qso]['dec'], qso[index_qso]['z'], 
+                               wave[index_wave[0]:index_wave[1]], 
+                               flux[index_wave[0]:index_wave[1], index_qso],
+                               continuum[index_wave[0]:index_wave[1], index_qso],
+                               ivar[index_wave[0]:index_wave[1], index_qso])],
+                               dtype=outstr_dtype)
+           fits = fitsio.FITS(outfile, 'rw', clobber=overwrite)
+           fits.write(outstr)
+           fits.close()
+
+
+def hstfos_qso_filename():
+    path = datapath.hstfos_path()
+    filename = 'HSTFOS_qso.fits'
+    return join(path, filename)
+
+
+def hstfos_qso_readin():
+    infile = hstfos_qso_filename()
+    return fitsio.read(infile)
+
+
+def hstfos_rest_allqsospec(overwrite=False):
+    """Load and interpolate *ALL* HST FOS quasar spectra
+
+    on to the same rest-frame wavelength grid
+    """
+    pbar = ProgressBar()
+
+    # check output files
+    
+    outfiles = hstfos_allinone_restframe_allfiles()
+    for thisfile in outfiles:
+        if isfile(thisfile) and not overwrite:
+           print "At least one of the files exists. Use overwrite to overwrite it."
+           return None
+    print "Will write into these files: {0}".format(outfiles)
+
+    # read in quasars
+    qso = hstfos_qso_readin() 
+
+    # read in master wavelength grid
+    master_wave = (allinone_wave_readin())['wave']
+    master_loglam = np.log10(master_wave)
+    nwave = master_wave.size()
+
+    rest_allflux = np.zeros((nwave, nqso))
+    rest_allcont = np.zeros((nwave, nqso))
+    rest_allivar = np.zeros((nwave, nqso))
+
+    channel = ['h130', 'h190', 'h270']
+    for i in np.arange(nqso):
+        # Progress bar
+        pbar.update(i+1)
+        tmpz = qso[i].z
+        wave_pos = np.array([1100./(1.+tmpz), 3350./(1.+tmpz)])
+        rest_loc = np.searchsorted(master_wave, wave_pos)
+        for j in np.arange(3):
+            tmpspec = hstfos_readspec(qso[i], channel[j], status=status)
+            if status==1:
+               tmploglam = np.log10(tmpspec['wave']/(1.+tmpz))
+               tmpflux = tmpspec['flux']/(1.+tmpz)
+               tmpcont = tmpspec['continuum']/(1.+tmpz)
+               tmpivar = 1./pow(tmpspec['error'], 2)/(1.+tmpz)
+               tmpivar = np.where(np.isfinite(tmpivar), tmpivar, 0.0)
+
+               # Interpolation, Flux
+               finterp, iinterp = combine1fiber(tmploglam, tmpflux, 
+                   objivar=tmpivar, newloglam=master_loglam[rest_loc[0]:rest_loc[1]])
+               tmp_outflux[:,j] = finterp
+               tmp_outivar[:,j] = iinterp
+               # Interpolation, Continuum
+               cinterp, ciinterp = combine1fiber(tmploglam, tmpcont, 
+                   objivar=tmpivar, newloglam=master_loglam[rest_loc[0]:rest_loc[1]])
+               tmp_outcont[:,j] = cinterp
+
+        # Coadd
+        tmp_allflux, tmp_allivar      = simple_coadd(tmp_outflux, 1, weight=tmp_outivar)
+        tmp_allcont, tmp_allivar_cont = simple_coadd(tmp_outcont, 1, weight=tmp_outivar)
+
+        # output
+        rest_allflux[rest_loc[0]:rest_loc[1],i] = tmp_allflux*10. # 10 for 10^(-16) to 10^(-17)
+        rest_allcont[rest_loc[0]:rest_loc[1],i] = tmp_allcont*10. # 10 for 10^(-16) to 10^(-17)
+        rest_allivar[rest_loc[0]:rest_loc[1],i] = tmp_allivar/100.
+
+    # write out
+    hstfos_allinone_rest_writeout(master_wave, rest_allflux, rest_allcont, rest_allivar)
 
